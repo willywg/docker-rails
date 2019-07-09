62cafc680ccfa3d1d396144882d8d2ad6be5691b
WillyWG <willywg@gmail.com>
2019-07-08 19:54:46 -0500

    Configure generators

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,16 @@ module App
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.test_framework :rspec,
+        fixtures: false,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        controller_specs: false 
+    end
   end
 end
