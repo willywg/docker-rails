b7ddade476efaa0cd0570a1413c13ce4e99b3fca
WillyWG <willywg@gmail.com>
2019-07-09 21:29:15 -0500

    Configure webpacker folders

---
+++ app/webpacker/images/index.js
@@ -0,0 +1,8 @@
+/*
+Source: https://medium.com/@coorasse/goodbye-sprockets-welcome-webpacker-3-0-ff877fb8fa79
+*/
+
+// import './image1.svg'
+// import './image2.png'
+// import './image3.jpg'
+// import './icon.png'
