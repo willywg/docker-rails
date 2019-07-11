c144610547f6810a8fb7574e44e7e6aa3eb14a81
WillyWG <willywg@gmail.com>
2019-07-11 00:20:12 -0500

    Home with boostrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,6 +17,7 @@
 import Rails from 'rails-ujs';
 import Turbolinks from 'turbolinks';
 import 'bootstrap/dist/js/bootstrap';
+require('../stylesheets/application.scss');
 
 Rails.start();
 Turbolinks.start();
